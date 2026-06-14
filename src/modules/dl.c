a314f9dd06b4d8b487015378ffdd3f5fe97bc789
Balazs Ruzsa <ruzsa.balazs@gmail.com>
2026-06-14 09:16:31 +0200

    revert dl changes

--- src/modules/dl.c
+++ src/modules/dl.c
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if defined(__unix__) || defined(__APPLE__)
+#if defined(_WIN32)
+#include <windows.h>
+#elif defined(__unix__) || defined(__APPLE__)
 #include <dlfcn.h>
 #endif
 
@@ -13,8 +15,101 @@ static void set_int_constant(lua_State *L, const char *name, int value) {
   lua_setfield(L, -2, name);
 }
 
+#if defined(_WIN32)
+
+static char rig_dl_last_error[512];
+
+static void rig_dl_set_windows_error_message(const char *prefix) {
+  DWORD code = GetLastError();
+  DWORD flags = FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS;
+  DWORD rc = FormatMessageA(flags, NULL, code, 0, rig_dl_last_error,
+                            (DWORD)sizeof(rig_dl_last_error), NULL);
+
+  if (rc == 0) {
+    _snprintf(rig_dl_last_error, sizeof(rig_dl_last_error),
+              "%s (Windows error %lu)", prefix, (unsigned long)code);
+    rig_dl_last_error[sizeof(rig_dl_last_error) - 1] = '\0';
+    return;
+  }
+
+  while (rc > 0 &&
+         (rig_dl_last_error[rc - 1] == '\r' || rig_dl_last_error[rc - 1] == '\n' ||
+          rig_dl_last_error[rc - 1] == ' ')) {
+    rig_dl_last_error[rc - 1] = '\0';
+    rc--;
+  }
+
+  {
+    char message[sizeof(rig_dl_last_error)];
+    strncpy(message, rig_dl_last_error, sizeof(message));
+    message[sizeof(message) - 1] = '\0';
+    _snprintf(rig_dl_last_error, sizeof(rig_dl_last_error), "%s: %s", prefix,
+              message);
+    rig_dl_last_error[sizeof(rig_dl_last_error) - 1] = '\0';
+  }
+}
+
+static wchar_t *rig_dl_utf8_to_utf16(const char *text) {
+  int length;
+  wchar_t *buffer;
+
+  if (text == NULL) {
+    return NULL;
+  }
+
+  length = MultiByteToWideChar(CP_UTF8, 0, text, -1, NULL, 0);
+  if (length <= 0) {
+    rig_dl_set_windows_error_message("failed to convert UTF-8 path to UTF-16");
+    return NULL;
+  }
+
+  buffer = (wchar_t *)malloc((size_t)length * sizeof(wchar_t));
+  if (buffer == NULL) {
+    strncpy(rig_dl_last_error, "out of memory", sizeof(rig_dl_last_error));
+    rig_dl_last_error[sizeof(rig_dl_last_error) - 1] = '\0';
+    return NULL;
+  }
+
+  if (MultiByteToWideChar(CP_UTF8, 0, text, -1, buffer, length) <= 0) {
+    free(buffer);
+    rig_dl_set_windows_error_message("failed to convert UTF-8 path to UTF-16");
+    return NULL;
+  }
+
+  return buffer;
+}
+
+#endif
+
 void *rig_dl_open(const char *path) {
-#if defined(__unix__) || defined(__APPLE__)
+#if defined(_WIN32)
+  HMODULE handle;
+  wchar_t *wpath;
+
+  if (path == NULL) {
+    handle = GetModuleHandleW(NULL);
+    if (handle == NULL) {
+      rig_dl_set_windows_error_message("failed to get current process module");
+      return NULL;
+    }
+    return (void *)handle;
+  }
+
+  wpath = rig_dl_utf8_to_utf16(path);
+  if (wpath == NULL) {
+    return NULL;
+  }
+
+  handle = LoadLibraryW(wpath);
+  free(wpath);
+
+  if (handle == NULL) {
+    rig_dl_set_windows_error_message("failed to load shared object");
+    return NULL;
+  }
+
+  return (void *)handle;
+#elif defined(__unix__) || defined(__APPLE__)
   dlerror();
   return dlopen(path, RTLD_NOW | RTLD_LOCAL);
 #else
@@ -24,7 +119,10 @@ void *rig_dl_open(const char *path) {
 }
 
 void *rig_dl_open_flags(const char *path, int flags) {
-#if defined(__unix__) || defined(__APPLE__)
+#if defined(_WIN32)
+  (void)flags;
+  return rig_dl_open(path);
+#elif defined(__unix__) || defined(__APPLE__)
   dlerror();
   return dlopen(path, flags);
 #else
@@ -35,7 +133,21 @@ void *rig_dl_open_flags(const char *path, int flags) {
 }
 
 void *rig_dl_sym(void *handle, const char *name) {
-#if defined(__unix__) || defined(__APPLE__)
+#if defined(_WIN32)
+  FARPROC symbol;
+
+  if (handle == NULL || name == NULL) {
+    return NULL;
+  }
+
+  symbol = GetProcAddress((HMODULE)handle, name);
+  if (symbol == NULL) {
+    rig_dl_set_windows_error_message("failed to load symbol");
+    return NULL;
+  }
+
+  return (void *)symbol;
+#elif defined(__unix__) || defined(__APPLE__)
   void *symbol;
 
   dlerror();
@@ -65,7 +177,13 @@ void *rig_dl_sym(void *handle, const char *name) {
 }
 
 int rig_dl_close(void *handle) {
-#if defined(__unix__) || defined(__APPLE__)
+#if defined(_WIN32)
+  if (FreeLibrary((HMODULE)handle) == 0) {
+    rig_dl_set_windows_error_message("failed to unload shared object");
+    return -1;
+  }
+  return 0;
+#elif defined(__unix__) || defined(__APPLE__)
   dlerror();
   return dlclose(handle);
 #else
@@ -75,7 +193,10 @@ int rig_dl_close(void *handle) {
 }
 
 const char *rig_dl_error(void) {
-#if defined(__unix__) || defined(__APPLE__)
+#if defined(_WIN32)
+  return rig_dl_last_error[0] != '\0' ? rig_dl_last_error
+                                      : "unknown dynamic loader error";
+#elif defined(__unix__) || defined(__APPLE__)
   return dlerror();
 #else
   return "dl module is not implemented on this platform";
